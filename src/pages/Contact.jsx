158373979f827cd254377cbbd6a9c837e160a4a5
Merve Yıldız <merveyildizzz41@gmail.com>
2022-10-11 15:14:42 +0300

    contact page has been completed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <Helmet title="Contact">
+            <CommonSection title="Contact" />
+            <div className="d-flex align-items-center justify-content-center flex-column mt-5">
+                <h5>Phone: 0262 123 45 56</h5>
+                <h5>Address: Kocaeli/Darıca</h5>
+            </div>
+        </Helmet>
     )
 }
 
